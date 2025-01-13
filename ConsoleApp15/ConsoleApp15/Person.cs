2ccfffdaface5051b95647277211806c33618f71
cagatay246 <cagataykilgi7@gmail.com>
2025-01-13 09:58:01 +0300

    1

---
+++ ConsoleApp15/ConsoleApp15/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Person
+    {
+        // Özellikler
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public DateTime DogumTarihi { get; set; }
+
+        // Yapıcı metot (constructor)
+        public Person(string ad, string soyad, DateTime dogumTarihi)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            DogumTarihi = dogumTarihi;
+        }
+
+        // Bilgileri yazdıran metot
+        public void BilgileriYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}, Doğum Tarihi: {DogumTarihi.ToShortDateString()}");
+        }
+    }
+
+
+
+
+
+
+
+
+}
+
---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Öğrenci nesneleri oluşturuluyor
+        Person ogrenci1 = new Person("Ali", "Yılmaz", new DateTime(2005, 5, 15));
+        Person ogrenci2 = new Person("Ayşe", "Kara", new DateTime(2006, 8, 22));
+
+        // Öğretmen nesneleri oluşturuluyor
+        Person ogretmen1 = new Person("Mehmet", "Demir", new DateTime(1980, 3, 12));
+        Person ogretmen2 = new Person("Zeynep", "Aydın", new DateTime(1985, 11, 5));
+
+        // Öğrenci ve öğretmen bilgileri yazdırılıyor
+        Console.WriteLine("Öğrenciler:");
+        ogrenci1.BilgileriYazdir();
+        ogrenci2.BilgileriYazdir();
+
+        Console.WriteLine("\nÖğretmenler:");
+        ogretmen1.BilgileriYazdir();
+        ogretmen2.BilgileriYazdir();
+    }
+}
